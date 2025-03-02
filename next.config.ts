2594c198cb7e131f77f69f99c8495b8ef2437cb8
kolbeyang <kky2806@rit.edu>
2025-03-02 16:15:50 -0500

    fix: rewrite remove next config

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/habits/:path*",
-        destination: "https://habit-tracker-eight-eta.vercel.app/:path*",
-      },
-      {
-        source: "/time/:path*",
-        destination:
-          "https://time-diff-visualizer-git-main-kolbeyangs-projects.vercel.app/:path*",
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
