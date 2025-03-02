e5177adf29aee168b9d4823a08bc3c7615db34b1
kolbeyang <kky2806@rit.edu>
2025-03-01 20:11:15 -0500

    fix: try switching from next rewrites to vercel rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,19 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/habits/:path*",
+        destination: "https://habit-tracker-eight-eta.vercel.app/:path*",
+      },
+      {
+        source: "/time/:path*",
+        destination:
+          "https://time-diff-visualizer-git-main-kolbeyangs-projects.vercel.app/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
