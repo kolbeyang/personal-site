8fec387d7cdfe565194f5e6fec23679c0ccb5d61
kolbeyang <kky2806@rit.edu>
2025-04-27 17:17:17 -0400

    fix: image size issues

--- src/components/HomeV2/SelectedWorks/PhotoGallery.tsx
+++ src/components/HomeV2/SelectedWorks/PhotoGallery.tsx
@@ -25,17 +25,17 @@ const PhotoGallery = ({ project }: Props) => {
       <img
         src={imagePaths[selectedImageIndex]}
         alt={title}
-        className="w-auto object-cover h-full rounded-md"
+        className="w-fill object-cover h-auto rounded-md"
       />
       <div className="absolute bottom-0 left-0 flex gap-1 w-full justify-between p-1">
         <Button
-          className="p-4 md:p-[10] ring-white text-white bg-white/20 backdrop-blur-xs hover:bg-white hover:text-text-base"
+          className="p-4 md:p-[10] ring-white text-white bg-white/40 hover:bg-white hover:text-text-base"
           onClick={() => incrementImage(-1)}
         >
           <IconArrowLeft className="size-[40px] md:size-[60px]" stroke={0.5} />
         </Button>
         <Button
-          className="p-4 md:p-[10] ring-white text-white bg-white/20 backdrop-blur-xs hover:bg-white hover:text-text-base"
+          className="p-4 md:p-[10] ring-white text-white bg-white/40 hover:bg-white hover:text-text-base"
           onClick={() => incrementImage(1)}
         >
           <IconArrowRight className="size-[40px] md:size-[60px]" stroke={0.5} />
